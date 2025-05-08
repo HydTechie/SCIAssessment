a0ff63e0abce11defa4197abf6a12df3c3cb1abc
HydTechie <kphani_prasad@yahoo.com>
2025-05-08 12:21:25 +0530

    Add to my repo!

---
+++ ExcelAddIn2/ExcelAddIn2/CustomRibbon.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ExcelAddIn2
+{
+    partial class CustomRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public CustomRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.Actions = this.Factory.CreateRibbonGroup();
+            this.btnConvertAlpha = this.Factory.CreateRibbonButton();
+            this.btnUndo = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.Actions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.Actions);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // Actions
+            // 
+            this.Actions.Items.Add(this.btnConvertAlpha);
+            this.Actions.Items.Add(this.btnUndo);
+            this.Actions.Label = "Actions";
+            this.Actions.Name = "Actions";
+            // 
+            // btnConvertAlpha
+            // 
+            this.btnConvertAlpha.Label = "Convert2Alpha";
+            this.btnConvertAlpha.Name = "btnConvertAlpha";
+            this.btnConvertAlpha.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnConvertAlpha_Click);
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Label = "Undo";
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnUndo_Click);
+            // 
+            // CustomRibbon
+            // 
+            this.Name = "CustomRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.CustomRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.Actions.ResumeLayout(false);
+            this.Actions.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup Actions;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnConvertAlpha;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnUndo;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal CustomRibbon Ribbon1
+        {
+            get { return this.GetRibbon<CustomRibbon>(); }
+        }
+    }
+}
---
+++ ExcelAddIn2/ExcelAddIn2/CustomRibbon.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace ExcelAddIn2
+{
+    public partial class CustomRibbon
+    {
+        private readonly Dictionary<string, object> originalValues = new Dictionary<string, object>();
+        private void CustomRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnConvertAlpha_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Application application = Globals.ThisAddIn.Application;
+            Excel.Range selectedRange = application.Selection as Excel.Range;
+
+            if (selectedRange == null)
+            {
+                return;
+            }
+
+            originalValues.Clear(); // reset 
+
+            foreach (Excel.Range cell in selectedRange.Cells)
+            {
+                if (cell.Value2 != null )
+                {
+                    originalValues[cell.Address] = cell.Value2;
+                    //Clearing Non-Alphanumeric presence in the cell value 
+                    cell.Value2 = Regex.Replace(cell.Value2, "[^a-zA-Z0-9]", "");
+                }
+            }
+            
+        }
+
+        private void btnUndo_Click(object sender, RibbonControlEventArgs e)
+        {
+            Excel.Application app = Globals.ThisAddIn.Application;
+            Excel.Worksheet activeSheet = app.ActiveSheet;
+
+            foreach(var keyValue in originalValues)
+            {
+                // select current address (Key) in stored value
+                Excel.Range cell = activeSheet.Range[keyValue];
+                // restore stored value of corresponding address
+                cell.Value2 = keyValue.Value;   
+            }
+        }
+    }
+}
